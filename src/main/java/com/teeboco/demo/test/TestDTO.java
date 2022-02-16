242f1f28d70da84874921b4715ad167daeac0822
hwkim <ksi05503@gmail.com>
2022-02-16 20:45:11 +0900

    TEEBOCO project init

---
+++ src/main/java/com/teeboco/demo/test/TestDTO.java
@@ -0,0 +1,13 @@
+package com.teeboco.demo.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TestDTO {
+    private String testString;
+}
